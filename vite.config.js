499a9a59d9a27865eda4ce2ac06ca386a27fa2be
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2025-06-29 12:36:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: {
+      'react-refresh/babel': path.resolve('./node_modules/react-refresh/babel.js'),
+    },
+  },
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'cdm2.onrender.com',
+      'localhost',  // You can add localhost here for local development as well.
+    ],
+   // port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
+});
